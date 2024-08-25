c9d4eb0c3540d63f6ac4f4da0f9577c7b6cde05d
Haniche Zahir <zahir.haniche@inetum.com>
2024-08-25 17:24:18 +0200

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="p-2 border-t-2 border-gray-200">
-      <p className="text-center uppercase text-lg">Boxy - Coded by <a href="https://github.com/ZahirHaniche-dev/reactjs-filter-countries" className="underline">HANICHE Zahir</a>.</p>
-    </footer>
-  )
-}
